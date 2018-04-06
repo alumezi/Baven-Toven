8b2cb8e63fd51076a615eef7c73a33810377d1ae
alumezi <arbnorlumezi@gmail.com>
2018-04-06 13:30:17 +0200

    first commit

---
+++ test.js
@@ -0,0 +1,25 @@
+document.getElementById('italiano').addEventListener('click', function(){
+    document.getElementById('subTranslation').innerHTML = "Dove e la vita";
+    document.getElementById('prenota').innerHTML = "Prenota Ora!";
+    document.getElementById('prenotazione').innerHTML = "Prenota Ora in: ";
+;})
+
+
+
+document.getElementById('english').addEventListener('click', function(){
+    document.getElementById('subTranslation').innerHTML = "Is where life happens";
+    document.getElementById('prenota').innerHTML = "Book Now!";
+    document.getElementById('prenotazione').innerHTML = "Book Now in: ";
+;})
+
+
+    $('#prenota').click(function(event) {
+    
+        event.preventDefault();
+
+        let n = $(document).height();
+        $('html, body').animate({ scrollTop: n }, 1000);
+
+    });
+
+    
